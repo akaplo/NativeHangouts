7989e307e4f5e0f4319ec9cbbbd58b23ea8a39d2
Aaron Kaplowitz <akaplowitz@umass.edu>
2015-10-15 22:27:32 -0400

    Initial Commit

---
+++ Hangouts/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hangouts
+//
+//  Created by Aaron on 10/15/15.
+//  Copyright © 2015 Aaron. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hangouts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hangouts
+//
+//  Created by Aaron on 10/15/15.
+//  Copyright © 2015 Aaron. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
